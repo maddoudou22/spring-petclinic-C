5ba2a8a6ad92ae43ac157bd0d9ab9dc986bdb600
user <you@example.com>
2019-05-26 14:53:03 +0200

    first commit

---
+++ Git Push.bat
@@ -0,0 +1,16 @@
+
+echo off
+
+SET nom_Projet_Git=spring-petclinic-C
+SET URL_Projet_Git=https://github.com/maddoudou22/spring-petclinic-C.git
+SET Repertoire_Local_Projet=C:\awsLambda\java\spring-petclinic-C
+cd %Repertoire_Local_Projet%
+
+echo "Git Add ..."
+git add -A
+
+echo "Git Commit ..."
+git commit -m "latest push from windows"
+
+echo "Git Push ..."
+git push %URL_Projet_Git%
